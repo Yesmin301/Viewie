bd1f2497eb25fad2cc9e110c07c4feac8e1b95fb
Yesmin301 <yesminhernandezreyna@gmail.com>
2024-10-18 16:30:43 -0500

    Colors
    Added some colors to gui.

--- Viewie/ViewieGUI.py
+++ Viewie/ViewieGUI.py
@@ -2,27 +2,32 @@ from tkinter import *
 #windo of the actual GUI
 root = Tk()
 root.title("Viewie")
+
 root.geometry("1030x920")
+root.configure(bg="maroon")
 
 #Label aka title.
-label = Label(root, text="Welcome to Viewie!", font=('Arial', 20))
+label = Label(root, text="Welcome to Viewie!", font=('Arial', 20,"bold",))
 label.pack(padx=20, pady=25)
-
+label.configure(bg="maroon")
+label.configure(foreground="White")
 
 frame = Frame(root)
 frame.pack()
-
+frame.configure(bg="Black")
 #here i made the text boxes 1 for uploading the code and the other for displaying the diagram.
 
 #Textbox1 is for python source code upload.
-textbox1 = Text(frame, height=15, width=30, font=('MonoLisa', 18))  
-textbox1.grid(row=0, column=0, padx=40)
+textbox1 = Text(frame, height=15, width=30, font=('MonoLisa', 18,"bold"))  
+textbox1.grid(row=0, column=0, padx=40,pady=10)
 textbox1.insert(END, "Placeholder text for Textbox 1")
+textbox1.configure(bg="Light grey")
 
 #Textbox2 is for "output" of source code -viewie .
-textbox2 = Text(frame, height=15, width=30, font=('MonoLisa', 18))  
-textbox2.grid(row=0, column=1, padx=10)
+textbox2 = Text(frame, height=15, width=30, font=('MonoLisa', 18,"bold"))  
+textbox2.grid(row=0, column=1, padx=40,pady=10)
 textbox2.insert(END, "Placeholder text for Textbox 2")
+textbox2.configure(bg="Light grey")
 
 #Making button to upload
 def upload_click():
